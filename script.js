e15a40e6b8c4a543aa9925c26707c6a549108fb9
Fred <68167225+Frednm1@users.noreply.github.com>
2022-05-06 17:46:14 -0300

    Pagamentos

---
+++ script.js
@@ -0,0 +1,80 @@
+$(function(){
+    $('#check').click(function(){
+        var x = document.getElementById("check")
+        var aviso = $('.termos')
+        if(x.checked){
+            aviso.fadeIn()
+            var aux = document.getElementById('submit-link')
+            aux.href = "{{route('pasta.view')}}"
+            botao = document.getElementById("b2")
+            botao.disabled = false
+        }else {
+            aviso.fadeOut()
+            botao = document.getElementById("b2")
+            botao.disabled = true
+        }
+    })
+    $('.submit').click(function(){
+
+        var x = document.getElementById("check")
+        if(x.checked == false)
+        
+            alert('É necessário aceitar as Condiçôes de Uso do Ligdoctor primeiro')
+        
+    })
+    
+
+})
+var select = document.getElementById("select")
+    function trocaPlano(){
+        var elemento1 = document.getElementById("elemento-plano1")
+        var elemento2 = document.getElementById("elemento-plano2")
+        var elemento3 = document.getElementById("elemento-plano3")
+        var elemento4 = document.getElementById("elemento-plano4")
+        current = select.value
+        if (current == 'Plano1'){
+            elemento2.classList.remove('visivel')
+            elemento3.classList.remove('visivel')
+            elemento4.classList.remove('visivel')
+
+            elemento2.classList.add('invisivel')
+            elemento3.classList.add('invisivel')
+            elemento4.classList.add('invisivel')
+
+            elemento1.classList.add('visivel')
+            elemento1.classList.remove('invisivel')
+        }else if(current == 'Plano2'){
+            elemento1.classList.remove('visivel')
+            elemento3.classList.remove('visivel')
+            elemento4.classList.remove('visivel')
+
+            elemento1.classList.add('invisivel')
+            elemento3.classList.add('invisivel')
+            elemento4.classList.add('invisivel')
+
+            elemento2.classList.add('visivel')
+            elemento2.classList.remove('invisivel')
+        }else if(current =='Plano3'){
+            elemento2.classList.remove('visivel')
+            elemento1.classList.remove('visivel')
+            elemento4.classList.remove('visivel')
+
+            elemento1.classList.add('invisivel')
+            elemento2.classList.add('invisivel')
+            elemento4.classList.add('invisivel')
+
+            elemento3.classList.add('visivel')
+            elemento3.classList.remove('invisivel')
+        }else if(current == 'Plano4'){
+            elemento2.classList.remove('visivel')
+            elemento3.classList.remove('visivel')
+            elemento1.classList.remove('visivel')
+
+            elemento2.classList.add('invisivel')
+            elemento3.classList.add('invisivel')
+            elemento1.classList.add('invisivel')
+
+            elemento4.classList.add('visivel')
+            elemento4.classList.remove('invisivel')
+        }
+    }
